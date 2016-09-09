505c9a3c9fddcd859c52f87869875d4b5b283b66
Aaron <aaronzimmermann92@gmail.com>
2016-09-09 17:17:20 +1000

    update

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ server.post('/api/messages', connector.listen());
 // Server frontpage
 //=========================================================
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
